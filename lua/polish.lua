686eb0dbd70bdf7a9c5e4c5223bad0ce1920fe01
Bruno Dal Bo <brunodalbo@google.com>
2025-06-25 17:13:13 -0700

    bring back what matters from previous config

--- lua/polish.lua
+++ lua/polish.lua
@@ -1,5 +1,16 @@
-if true then return end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+function BrunoAutoCmd()
+  local wkl = require "which-key"
+  local fileTy = vim.api.nvim_buf_get_option(0, "filetype")
 
--- This will run last in the setup process.
--- This is just pure lua so anything that doesn't
--- fit in the normal config locations above can go here
+  if fileTy == "gitrebase" then
+    wkl.register {
+      ["R"] = { "^dwireword <ESC>", "Reword" },
+      ["E"] = { "^dwiedit <ESC>", "Edit" },
+      ["B"] = { "Obreak<ESC>", "Break" },
+    }
+  elseif fileTy == "gitcommit" then
+    vim.opt.colorcolumn = { "51", "73" }
+  end
+end
+
+vim.cmd "autocmd FileType * lua BrunoAutoCmd()"
