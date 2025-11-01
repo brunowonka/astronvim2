978101eb8c47be96b35b45340c0b5edb79f2c41d
Bruno Dal Bo <brunodalbo@google.com>
2025-10-31 21:18:04 -0700

    disable silly jj escape

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -1,4 +1,8 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+if true then
+  return {
+    { "max397574/better-escape.nvim", enabled = false },
+  }
+end
 
 -- You can also add or configure plugins by creating files in this `plugins/` folder
 -- PLEASE REMOVE THE EXAMPLES YOU HAVE NO INTEREST IN BEFORE ENABLING THIS FILE
